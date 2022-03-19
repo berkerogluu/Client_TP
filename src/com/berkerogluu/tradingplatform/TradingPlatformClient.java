7d2b8bd3d21198d5324d592b4757c86203eda079
Scispow <scispow@gmail.com>
2022-03-19 21:34:51 +0300

    Update

--- src/com/berkerogluu/tradingplatform/TradingPlatformClient.java
+++ src/com/berkerogluu/tradingplatform/TradingPlatformClient.java
@@ -4,11 +4,12 @@ import com.berkerogluu.tradingplatform.controller.Controller;
 import com.berkerogluu.tradingplatform.view.*;
 
 public class TradingPlatformClient {
+
     public static void main(String args[]){
+
         MainView mMainView = new MainView();
+
         Controller mController = new Controller(mMainView);
         mController.initController();
-
-        //mMainView.getFrame().setTitle("test");
     }
 }
--- src/com/berkerogluu/tradingplatform/view/FrameView.java
+++ src/com/berkerogluu/tradingplatform/view/FrameView.java
@@ -1,14 +1,15 @@
 package com.berkerogluu.tradingplatform.view;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class FrameView extends JFrame{
 
     public FrameView(){
-        // Frame
         this.setTitle("Trading Platform");
         this.setLayout(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(new Dimension(600,400));
         this.setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         this.setVisible(true);
     }
